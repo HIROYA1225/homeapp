73d56900e2683ee4926c9ce98340536ee1e7973c
hiro <area0466.d25@gmail.com>
2023-08-08 22:26:08 +0900

    Initial Commit

---
+++ homeapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  homeapp
+//
+//  Created by HIRO on 2023/08/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ homeapp/homeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeappApp.swift
+//  homeapp
+//
+//  Created by HIRO on 2023/08/08.
+//
+
+import SwiftUI
+
+@main
+struct homeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
