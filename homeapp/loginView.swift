59a75323dd4f25a8e182c1852e78afd87d468e29
HIRO <area0466.d25@gmail.com>
2023-09-18 17:55:32 +0900

    ログイン画面レイアウト調整

--- homeapp/loginView.swift
+++ homeapp/loginView.swift
@@ -10,49 +10,60 @@ import SwiftUI
 struct loginView: View {
     @State private var email = ""
     @State private var password = ""
+    @State private var active = false
     var body: some View {
-        VStack(){
-            Text("褒めアプテスト")
-                .font(.largeTitle).foregroundColor(Color.black)
-                .padding([.top, .bottom], 40)
-            TextField("Email", text:self.$email)
-                .padding()
-                .cornerRadius(20.0)
-            TextField("Password", text:self.$password)
-                .padding()
-                .cornerRadius(20.0)
-            
-            HStack(){
-                Button(action:{}){
-                    Image("Google_icon")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                }
-                Button(action:{}){
-                    Image("LINE_icon")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                }
-            }
-            HStack(){
-                Button(action:{}){
-                    Text("Sign In")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.green)
-                        .cornerRadius(15.0)
-                }
-                Button(action:{}){
-                    Text("新規登録")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.green)
-                        .cornerRadius(15.0)
-                }
+        NavigationStack{
+            VStack(alignment: .center){
+                Text("褒めアプテスト")
+                    .font(.largeTitle).foregroundColor(Color.black)
+                    .padding([.top, .bottom], 40)
+                TextField("Email", text:self.$email)
+                    .padding()
+                    .cornerRadius(20.0)
+                TextField("Password", text:self.$password)
+                    .padding()
+                    .cornerRadius(20.0)
                 
+                HStack(spacing: 20){
+                    Button(action:{}){
+                        Image("Google_icon")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                    }
+                    Button(action:{}){
+                        Image("LINE_icon")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                    }
+                }
+                .padding(.bottom,10)
+                .offset(x:-5)
+                HStack(){
+                    Button(action:{
+                        active.toggle()
+                    }){
+                        Text("Sign In")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.green)
+                            .cornerRadius(15.0)
+                    }
+                    .navigationDestination(isPresented: $active, destination: {
+                        registerView()
+                    })
+                    Button(action:{}){
+                        Text("新規登録")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.green)
+                            .cornerRadius(15.0)
+                    }
+                    
+                }
             }
+
         }
     }
 }
