9b580b158b1fe34fbb4c2fdb2eacf46b179bdd4c
HIRO <area0466.d25@gmail.com>
2023-09-24 17:17:37 +0900

    ページ遷移追加

--- homeapp/loginView.swift
+++ homeapp/loginView.swift
@@ -10,48 +10,65 @@ import SwiftUI
 struct loginView: View {
     @State private var email = ""
     @State private var password = ""
+    @State var path = NavigationPath()
     var body: some View {
-        VStack(){
-            Text("褒めアプテスト")
-                .font(.largeTitle).foregroundColor(Color.black)
-                .padding([.top, .bottom], 40)
-            TextField("Email", text:self.$email)
-                .padding()
-                .cornerRadius(20.0)
-            TextField("Password", text:self.$password)
-                .padding()
-                .cornerRadius(20.0)
-            
-            HStack(){
-                Button(action:{}){
-                    Image("Google_icon")
-                        .resizable()
-                        .frame(width: 30, height: 30)
+        NavigationStack(path: $path){
+            VStack(){
+                Text("褒めアプテスト")
+                    .font(.largeTitle).foregroundColor(Color.black)
+                    .padding([.top, .bottom], 40)
+                TextField("Email", text:self.$email)
+                    .padding()
+                    .cornerRadius(20.0)
+                TextField("Password", text:self.$password)
+                    .padding()
+                    .cornerRadius(20.0)
+                
+                HStack(){
+                    Button(action:{}){
+                        Image("Google_icon")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                    }
+                    Button(action:{}){
+                        Image("LINE_icon")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                    }
                 }
-                Button(action:{}){
-                    Image("LINE_icon")
-                        .resizable()
-                        .frame(width: 30, height: 30)
+                HStack(){
+                    Button(action:{
+                        path.append("toLoginCheck")
+                    }){
+                        Text("Sign In")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.green)
+                            .cornerRadius(15.0)
+                    }
+                    Button(action:{
+                        path.append("toRegister")
+                    }){
+                        Text("新規登録")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.green)
+                            .cornerRadius(15.0)
+                    }
+                    
                 }
             }
-            HStack(){
-                Button(action:{}){
-                    Text("Sign In")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.green)
-                        .cornerRadius(15.0)
+            .navigationDestination(for:String.self){ destination in
+                switch destination {
+                case "toLoginCheck":
+                    homeView()
+                case "toRegister":
+                    registerView()
+                default:
+                    loginView()
                 }
-                Button(action:{}){
-                    Text("新規登録")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.green)
-                        .cornerRadius(15.0)
-                }
-                
             }
         }
     }
