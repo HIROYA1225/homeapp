91cf5ccaf9b6fe54dc3b20ad3c21d672f2a8e359
HIRO <area0466.d25@gmail.com>
2023-11-21 21:57:36 +0900

    RegistPremium追加

---
+++ homeapp/RegistPremium.swift
@@ -0,0 +1,15 @@
+//
+//  RegistPremium.swift
+//  homeapp
+//
+//  Created by HIRO on 2023/11/21.
+//
+
+import Foundation
+import SwiftUI
+struct RegisterPremium: View {
+    var body: some View {
+        Text("プレミアム会員登録画面")
+            .background(Color.yellow)
+    }
+}
