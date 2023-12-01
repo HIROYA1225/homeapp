ba09cccc611c61680aef508c289c89caf65dc33a
yosukek0729 <yosukek0729@gmail.com>
2023-12-01 21:13:24 +0900

    RegistPremium追加

---
+++ homeapp/MembershipView.swift
@@ -0,0 +1,18 @@
+//
+//  MembershipView.swift
+//  homeapp
+//
+//  Created by YOSUKE on 2023/11/24.
+//
+
+import SwiftUI
+
+struct MembershipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MembershipView()
+}
